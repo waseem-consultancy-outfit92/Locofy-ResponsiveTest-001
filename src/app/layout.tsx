ea6ec826dced5425e7b20510e34f7395a70a2e10
hafizkashifdev <kashifnazim127@gmail.com>
2025-07-21 12:44:18 +0500

    og iamge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,11 +36,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: "CQC Process Lifecycle",
     description: "This visual guide breaks down the CQC process, outlining who must register, how the registration works, and the key compliance steps involved for care providers.",
-    url: "",
+    url: "https://carequalitycommission.netlify.app/",
     type: "website",
     images: [
       {
-        url: "/og.png",
+        url: "https://carequalitycommission.netlify.app/og.png",
         type: "image/png",
         width: 1200,
         height: 630,
