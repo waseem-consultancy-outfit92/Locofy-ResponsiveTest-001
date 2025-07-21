311798dbf2427fd775548e42ccba2356d29afc0f
hafizkashifdev <kashifnazim127@gmail.com>
2025-07-21 12:25:44 +0500

    Fix: removed secrets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             gutterBottom
             sx={{ color: "#003366" }}
           >
-            Welcome to the starter template Locofy
+            Welcome to the AQSE
           </Typography>
           <Typography variant="h6" sx={{ color: "#555" }}>
             Your trusted platform for care service oversight and reporting.
