aee883d843f30c3fc5735d1f6361719c4ec097f9
I'm Batman <auns.mehmood@consultancyoutfit.co.uk>
2025-07-22 11:06:32 +0500

    first page

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -6,3 +6,12 @@ export { default as CqcFundamentalStandardsImage } from "./CqcFundamentalStandar
 export { default as img1ScopeOfRegistrationImage } from "./img1ScopeOfRegistrationImage.svg";
 export { default as img2ScopeOfRegistrationImage } from "./img2ScopeOfRegistrationImage.svg";
 export { default as img3ScopeOfRegistrationImage } from "./img3ScopeOfRegistrationImage.svg";
+export { default as img1LegalAgreementsImage } from "./img1LegalAgreementsImage.svg";
+export { default as img2LegalAgreementsImage } from "./img2LegalAgreementsImage.svg";
+export { default as img3LegalAgreementsImage } from "./img3LegalAgreementsImage.svg";
+export { default as img4LegalAgreementsImage } from "./img4LegalAgreementsImage.svg";
+export { default as img5LegalAgreementsImage } from "./img5LegalAgreementsImage.svg";
+export { default as img6LegalAgreementsImage } from "./img6LegalAgreementsImage.svg";
+export { default as img7LegalAgreementsImage } from "./img7LegalAgreementsImage.svg";
+export { default as img8LegalAgreementsImage } from "./img8LegalAgreementsImage.svg";
+export { default as img9LegalAgreementsImage } from "./img9LegalAgreementsImage.svg";
