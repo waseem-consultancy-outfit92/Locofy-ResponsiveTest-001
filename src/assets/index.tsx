249ff7e04afe52e13401f39fe436acd93ad9d236
talha shah <talha.shah@ceative.co.uk>
2025-07-22 15:43:54 +0500

    feat:add aqse document page

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -6,3 +6,9 @@ export { default as CqcFundamentalStandardsImage } from "./CqcFundamentalStandar
 export { default as img1ScopeOfRegistrationImage } from "./img1ScopeOfRegistrationImage.svg";
 export { default as img2ScopeOfRegistrationImage } from "./img2ScopeOfRegistrationImage.svg";
 export { default as img3ScopeOfRegistrationImage } from "./img3ScopeOfRegistrationImage.svg";
+export { default as aqseDocument1Image } from "./aqseDocument1Image.svg";
+export { default as aqseDocument2Image } from "./aqseDocument2Image.svg";
+export { default as aqseDocument3Image } from "./aqseDocument3Image.svg";
+export { default as aqseDocument4Image } from "./aqseDocument4Image.svg";
+export { default as aqseDocument5Image } from "./aqseDocument5Image.svg";
+export { default as aqseDocument6Image } from "./aqseDocument6Image.svg";
