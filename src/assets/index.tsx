84c3cba3105d8504749d6d77a39b4721808e95e8
Zaka Ullah <zaka.ullah@consultancyoutfit.co.uk>
2025-07-22 12:13:37 +0500

    feat: add aqse overview page

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -6,3 +6,4 @@ export { default as CqcFundamentalStandardsImage } from "./CqcFundamentalStandar
 export { default as img1ScopeOfRegistrationImage } from "./img1ScopeOfRegistrationImage.svg";
 export { default as img2ScopeOfRegistrationImage } from "./img2ScopeOfRegistrationImage.svg";
 export { default as img3ScopeOfRegistrationImage } from "./img3ScopeOfRegistrationImage.svg";
+export { default as aqseOverviewImage } from "./aqseOverviewImage.svg";
