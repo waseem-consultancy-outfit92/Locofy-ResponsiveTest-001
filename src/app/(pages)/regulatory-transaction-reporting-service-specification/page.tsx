d6071e17ab404900ca42eb9a7fb6726fb13fe810
I'm batman <auns.mehmood@consultancyoutfit.co.uk>
2025-07-23 12:56:57 +0500

    feat::regulartory and short code complete

--- src/app/(pages)/regulatory-transaction-reporting-service-specification/page.tsx
+++
@@ -1,8 +0,0 @@
-import { RegulatoryTranscation } from "@/features/regulatory-transcation";
-import React from "react";
-
-const RegulatoryTransactionReportingServiceSpecificationPage = () => {
-  return <RegulatoryTranscation />;
-};
-
-export default RegulatoryTransactionReportingServiceSpecificationPage;
