34b97317a6c303d0d4c99798b547b6a561729ecb
shayan <Shayan.ulhaq@consultancyoutfit.co.uk>
2025-07-28 11:17:52 +0500

    added some new changes in code

--- src/app/(pages)/ig-required-affiliation/page.tsx
+++ src/app/(pages)/ig-required-affiliation/page.tsx
@@ -10,7 +10,7 @@ import {
     Stack
 } from '@mui/material';
 import Link from 'next/link';
-import { colorLegends, CommonBackIcon } from '@/assets/common-assets';
+import { colorLegends1, CommonBackIcon } from '@/assets/common-assets';
 import { IGRequiredAffiliationImage } from '@/assets';
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
@@ -144,7 +144,7 @@ const IGRequiredAffiliation = () => {
                                 onClick={(e) => e.stopPropagation()}
                             >
                                 <Image
-                                    src={colorLegends}
+                                    src={colorLegends1}
                                     alt="Color Legend"
                                     width={300}
                                     height={200}
